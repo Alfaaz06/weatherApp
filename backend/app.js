eec1c51e0fa90053fc6e2a86a13b261e67b07d04
Alfaaz06 <abhaybhaskar2001@gmail.com>
2023-06-06 19:25:38 +0530

    Second Commit

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ export const app = express();
 import Weather from './routes/weather.js';
 import User from './routes/user.js';
 import path from 'path';
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +13,8 @@ app.use(cookieParser());
 app.use('/api/v1', Weather);
 app.use('/api/v1', User);
 
-// app.use(express.static(path.resolve(" ./frontend/build")));
-// app.use(express.static(path.resolve("./frontend/build")));
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.resolve("./frontend/build/index.html"));
-// });
+app.use(express.static(path.resolve(" ./frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
